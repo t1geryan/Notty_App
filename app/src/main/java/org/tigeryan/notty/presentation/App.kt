0d9b20853e60bc19ffc0b1899a4e026b526c9955
t1geryan <sgta1980@gmail.com>
2023-07-20 00:07:41 +0300

    add dependencies for Kotlin Coroutines and Hilt
    implement App.kt with @HiltAndroidApp annotation
    
    register App.kt in AndroidManifest.xml
    
    add @AndroidEntryPoint annotation to MainActivity.kt

---
+++ app/src/main/java/org/tigeryan/notty/presentation/App.kt
@@ -0,0 +1,7 @@
+package org.tigeryan.notty.presentation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
--- app/src/main/java/org/tigeryan/notty/presentation/MainActivity.kt
+++ app/src/main/java/org/tigeryan/notty/presentation/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
 import org.tigeryan.notty.presentation.navigation.RootNavGraph
 import org.tigeryan.notty.presentation.theme.NottyTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
