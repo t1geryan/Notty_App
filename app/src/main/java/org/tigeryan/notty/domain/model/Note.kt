e8a9c53db69d12ffdd0ce89a68ac4340501af043
t1geryan <sgta1980@gmail.com>
2023-08-15 19:55:04 +0300

    [refactor] add java docs to Note.kt

--- app/src/main/java/org/tigeryan/notty/domain/model/Note.kt
+++ app/src/main/java/org/tigeryan/notty/domain/model/Note.kt
@@ -1,17 +1,27 @@
 package org.tigeryan.notty.domain.model
 
+data class Note(
+    val id: Long,
+    val noteData: NoteData,
+    val noteTime: NoteTime,
+)
+
+/**
+ * Note-specific data
+ * @param title notes title
+ * @param text  notes text
+ */
 data class NoteData(
     val title: String,
     val text: String,
 )
 
+/**
+ * Note-specific timestamps
+ * @param createdAt creation utc time in millis
+ * @param modifiedAt last modification utc time in millis
+ */
 data class NoteTime(
     val createdAt: Long,
     val modifiedAt: Long,
-)
-
-data class Note(
-    val id: Long,
-    val noteData: NoteData,
-    val noteTime: NoteTime,
 )
